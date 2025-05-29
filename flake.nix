6d62d91f12d76adfe8a84201cddbe9c28c3dfd52
Rishabh <40533251+Rishabh5321@users.noreply.github.com>
2025-05-29 12:40:51 +0530

    Update flake configuration and add package definition for GNU Hello

--- flake.nix
+++ flake.nix
@@ -1,21 +1,26 @@
 {
-  description = "Template flake for Nix projects";
+  description = "Flake with package defined in package.nix";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = { self, nixpkgs }: let
+    forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" ] (system:
+      f {
+        inherit system;
         pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        packages.default = pkgs.hello;
-        devShells.default = pkgs.mkShell {
-          buildInputs = [ pkgs.hello ];
-        };
       }
     );
+  in {
+    packages = forAllSystems ({ pkgs, ... }: {
+      default = pkgs.callPackage ./package.nix {};
+    });
+
+    defaultPackage = forAllSystems ({ pkgs, ... }: pkgs.callPackage ./package.nix {});
+
+    devShells = forAllSystems ({ pkgs, ... }: {
+      default = pkgs.mkShell {
+        buildInputs = [ pkgs.callPackage ./package.nix {} ];
+      };
+    });
+  };
 }
